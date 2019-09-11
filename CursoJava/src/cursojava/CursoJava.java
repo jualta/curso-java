8131fb390f935319b29c278969383e6eef0948cc
LAB202 <LAB202@LAB206-PC14>
2019-09-11 09:35:03 -0500

    primer depositorio

---
+++ CursoJava/src/cursojava/CursoJava.java
@@ -0,0 +1,13 @@
+
+package cursojava;
+
+public class CursoJava {
+
+    
+    public static void main(String[] args) {
+        
+        
+        System.out.println("mi primer depositorio");
+    }
+    
+}
